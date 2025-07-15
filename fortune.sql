c026355bcda8c4148c555b7c7d45c6f2acbf2957
Null <107396951+a-wow@users.noreply.github.com>
2025-07-15 14:19:03 +0300

    Update fortune.sql

--- fortune.sql
+++ fortune.sql
@@ -2,13 +2,18 @@ CREATE TABLE `items`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_items` int NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
-INSERT INTO `items` VALUES (1, 49623, 'Темная Скорбь');
-INSERT INTO `items` VALUES (2, 50675, 'Перчатки скрытности Алдрианы');
-INSERT INTO `items` VALUES (3, 35356, 'Перчатки из драконьей шкуры');
-INSERT INTO `items` VALUES (4, 36942, 'Ледяная Скорбь');
+INSERT INTO `items` VALUES (1, 49623, 'Темная Скорбь', '1.jpg');
+INSERT INTO `items` VALUES (2, 50675, 'Перчатки скрытности Алдрианы', '2.jpg');
+INSERT INTO `items` VALUES (3, 35356, 'Перчатки из драконьей шкуры', '3.jpg');
+INSERT INTO `items` VALUES (4, 36942, 'Ледяная Скорбь', '4.jpg');
+INSERT INTO `items` VALUES (5, 39769, 'Арканитовый потрошитель', '5.jpg');
+INSERT INTO `items` VALUES (6, 43601, 'Бруннхильдарский гигантский топор\r\n', '6.jpg');
+INSERT INTO `items` VALUES (7, 47069, 'Творец справедливости', '7.jpg');
+INSERT INTO `items` VALUES (8, 30316, 'Сокрушитель', '8.jpg');
 
 CREATE TABLE `sent_items`  (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -18,4 +23,4 @@ CREATE TABLE `sent_items`  (
   `quantity` int NOT NULL,
   `sent_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
